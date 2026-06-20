c923235f939cedda08a8d5ac09774ee5dabb48ba
RonaldBorda <ronald.bordab@gmail.com>
2026-06-20 10:20:52 -0500

    configurar pruebas automatizadas con github actions

--- src/test/java/tests/HP15LoginAdministradorExitosoTest.java
+++ src/test/java/tests/HP15LoginAdministradorExitosoTest.java
@@ -5,6 +5,9 @@ import pages.LoginPage;
 import pages.AdminPage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HP15LoginAdministradorExitosoTest extends BaseTest {
 
@@ -13,23 +16,34 @@ public class HP15LoginAdministradorExitosoTest extends BaseTest {
         try {
             System.out.println("CP-009: Login exitoso de Administrador");
 
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
             LoginPage loginPage = new LoginPage(driver);
             loginPage.abrirLogin();
             loginPage.iniciarSesion("admin", "admin");
 
             AdminPage adminPage = new AdminPage(driver);
 
-            Assertions.assertTrue(
-                    adminPage.panelAdministradorVisible(),
-                    "El Panel Administrador no se muestra después del inicio de sesión."
+            boolean panelAdministradorVisible = wait.until(d ->
+                    adminPage.panelAdministradorVisible()
             );
 
             Assertions.assertTrue(
-                    adminPage.estadisticasVisibles(),
+                    panelAdministradorVisible,
+                    "El Panel Administrador no se muestra después del inicio de sesión."
+            );
+
+            boolean estadisticasVisibles = wait.until(d ->
+                    adminPage.estadisticasVisibles()
+            );
+
+            Assertions.assertTrue(
+                    estadisticasVisibles,
                     "La sección de estadísticas no se muestra."
             );
 
             System.out.println("CP-009 ejecutado correctamente.");
+
         } catch (Exception e) {
             Assertions.fail("Error durante la ejecución de CP-009: " + e.getMessage());
         }
